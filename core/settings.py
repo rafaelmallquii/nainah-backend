efafec4532cdd5e2f8d60931f8df35294c471ecb
YotoCoder <support@yotodev.com>
2023-06-28 14:01:06 -0500

    asd

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
