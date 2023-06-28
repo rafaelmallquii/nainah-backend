a83784e1a38295a236458ae58b39b19f3517cbcf
YotoCoder <support@yotodev.com>
2023-06-28 14:01:36 -0500

    cors

--- core/settings.py
+++ core/settings.py
@@ -84,13 +84,14 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "corsheaders.middleware.CorsMiddleware",
-    "django.middleware.common.CommonMiddleware",
+    
+    
     # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
