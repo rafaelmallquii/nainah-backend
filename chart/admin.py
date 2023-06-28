5338b8158b9ff6b681637e0723dd648ee7366f03
YotoCoder <support@yotodev.com>
2023-06-27 21:19:49 -0500

    updated .gitignore

--- chart/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Chart
-# Register your models here.
-
-@admin.register(Chart)
-class ChartAdmin(admin.ModelAdmin):
-    # change_list_template = 'admin/chart/index.html'
-    pass
--- setting/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-from .models import Setting
-from .serializers import SettingSerializer
-
-class SettingViewSet(viewsets.ModelViewSet):
-    queryset = Setting.objects.all()
-    serializer_class = SettingSerializer
-    
-    http_method_names = ['get',]
