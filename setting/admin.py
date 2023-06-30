b285e7f96ad7a42045473dacd67671cadff5163a
YotoCoder <support@yotodev.com>
2023-06-30 16:02:37 -0500

    img pre

--- setting/admin.py
+++ setting/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import Setting, Currency, TaxAndShipment, SiteMeta
+import easy
 
 class InlineSiteMeta(admin.StackedInline):
     model = SiteMeta
@@ -15,9 +16,14 @@ class InlineCurrency(admin.StackedInline):
 
 @admin.register(Setting)
 class SettingAdmin(admin.ModelAdmin):
+    
     inlines = [
         InlineCurrency,
         InlineTax,
         InlineSiteMeta,
     ]
-    list_display = ('site_name', 'site_description', 'site_logo')
+    list_display = ('site_name', 'site_description', 'logo')
+    
+    @easy.with_tags()
+    def logo(self, obj):
+        return f'<img src="{obj.site_logo.url}" width="100px">'
