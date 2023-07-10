a9ae8c48e788b92758797ce092c8063799133a02
YotoCoder <support@yotodev.com>
2023-07-10 15:17:25 -0500

    +tags serializers

--- product/serializers.py
+++ product/serializers.py
@@ -7,15 +7,17 @@ class ProductVariantSerializer(serializers.ModelSerializer):
         model = ProductVariant
         fields = '__all__'
         
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = '__all__'
+        
 class ProductSerializer(serializers.ModelSerializer):
     category = CategorySerializer()
     variants = ProductVariantSerializer(many=True)
+    tags = TagSerializer(many=True)
     
     class Meta:
         model = Product
         fields = '__all__'
         
-class TagSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Tag
-        fields = '__all__'
